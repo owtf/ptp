04cb5de8c150e3d6dc900f4e2bc0536a9b28f4fc
Tao Sauvage <sauvage.tao@gmail.com>
2014-05-23 22:54:56 +0200

    [wapiti] Add signatures.

--- libptp/tools/wapiti/signatures.py
+++ libptp/tools/wapiti/signatures.py
@@ -12,7 +12,22 @@ from libptp.constants import HIGH, MEDIUM, LOW, INFO
 
 # TODO: Complete the signatures database.
 SIGNATURES = {
+    # High ranked vulnerabilities
     'SQL Injection': HIGH,
     'Blind SQL Injection': HIGH,
     'Command execution': HIGH,
+
+    # Medium ranked vulnerabilities
+    'Htaccess Bypass': MEDIUM,
+    'Cross Site Scripting': MEDIUM,
+    'CRLF Injection': MEDIUM,
+
+    # Low ranked vulnerabilities
+    'File Handling': LOW,  # a.k.a Path or Directory listing
+    'Resource consumption': LOW,  # TODO: Is this higher than LOW?
+
+    # Informational ranked vulnerabilities
+    'Backup file': INFO,
+    'Potentially dangerous file': INFO,  # TODO: Is this higher than INFO?
+    'Internal Server Error': INFO,
 }
