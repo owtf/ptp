51db51ecf271d7cd1a5f77b890ffd9119df2bd34
Tao Sauvage <sauvage.tao@gmail.com>
2014-07-08 16:22:18 +0200

    [metasploit] Add unit tests for scanner plugins.

--- tests/metasploit/test.py
+++ tests/metasploit/test.py
@@ -5,7 +5,7 @@ import traceback
 
 from ptp import PTP
 from libptp.tools.metasploit.report import MetasploitReport
-from libptp.constants import INFO, LOW, MEDIUM
+from libptp.constants import UNKNOWN, INFO, LOW, MEDIUM
 
 
 __testname__ = 'metasploit'
@@ -36,7 +36,60 @@ REPORTS = {
     'auxiliary/scanner/vnc/vnc_none_auth': {
         'report_medium.metasploit': MEDIUM,
     },
+    'auxiliary/scanner/x11/open_x11': {
+        'report_medium.metasploit': MEDIUM,
+        'report_medium2.metasploit': MEDIUM,
+    },
+    # TODO: Add report examples for EMC AlphaStor.
+    'auxiliary/scanner/mssql/mssql_ping': {
+        'report_info.metasploit': INFO,
+        'report_info2.metasploit': INFO,
+        'report_info3.metasploit': INFO,
+    },
+    'auxiliary/scanner/mssql/mssql_login': {
+        'report_medium.metasploit': MEDIUM,
+        'report_medium2.metasploit': MEDIUM,
+    },
+    'auxiliary/scanner/mssql/mssql_hashdump': {
+        'report_medium.metasploit': MEDIUM,
+    },
+    # TODO: Add report examples for MSSQL Schema dump.
+    # TODO: Add report examples for DCERPC endpoint mapper.
+    # TODO: Add report examples for DCERPC hidden.
+    'auxiliary/scanner/smb/smb_version': {
+        'report_info.metasploit': INFO,
+        'report_info2.metasploit': INFO,
+        'report_info3.metasploit': INFO,
+    },
+    'auxiliary/scanner/smb/pipe_auditor': {
+        'report_info.metasploit': INFO,
+        'report_info2.metasploit': INFO,
+    },
+    'auxiliary/scanner/smb/smb_enumusers': {
+        'report_info.metasploit': INFO,
+        'report_info2.metasploit': INFO,
+        'report_info3.metasploit': INFO,
+    },
+    'auxiliary/scanner/smb/smb_login': {
+        'report_medium.metasploit': MEDIUM,
+        'report_medium2.metasploit': MEDIUM,
+        'report_unknown.metasploit': UNKNOWN,
+    },
+    'auxiliary/scanner/snmp/snmp_enumusers': {
+        'report_info.metasploit': INFO,
+    },
+    'auxiliary/scanner/snmp/snmp_enumshares': {
+        'report_info.metasploit': INFO,
+    },
+    # TODO: Add report examples for SNMP enums.
+    # TODO: Add report examples for SNMP AIX version.
+    'auxiliary/scanner/snmp/snmp_login': {
+        'report_info.metasploit': INFO,
+        'report_medium.metasploit': MEDIUM,
+        'report_medium2.metasploit': MEDIUM,
+    }
 }
+
 
 def run():
     try:
@@ -52,7 +105,10 @@ def run():
             res = 'ok'
             try:
                 ptp.parse(
-                    pathname=os.path.join(os.getcwd(), 'tests/metasploit/', plugin),
+                    pathname=os.path.join(
+                        os.getcwd(),
+                        'tests/metasploit/',
+                        plugin),
                     filename=output,
                     plugin=plugin)
             except Exception:
