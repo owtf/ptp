77eee4dae413603b2cf255bfbc7ed5f8b6c3404d
Tao Sauvage <sauvage.tao@gmail.com>
2014-08-14 03:39:14 +0200

    [parser] Update docstrings.

--- libptp/parser.py
+++ libptp/parser.py
@@ -13,7 +13,15 @@ from lxml.etree import LxmlError
 
 
 class AbstractParser(object):
-    """Abstract representation of a parser."""
+
+    """Abstract representation of a parser.
+
+    .. note::
+
+        This class will be extended for each pentesting tool. That way, each
+        tool will add its own parsing specificities.
+
+    """
 
     #: :class:`str` -- Name of the tool.
     __tool__ = None
@@ -25,41 +33,46 @@ class AbstractParser(object):
     def __init__(self, fullpath):
         """Initialize AbstractParser.
 
-        :param str pathname: path to the report file.
+        :param str fullpath: full path to the report file.
 
         """
-        #: i/o stream -- i/o stream of the report.
-        self.stream = self.handle_file(pathname)
+        #: :class:`type` -- i/o stream of the report.
+        self.stream = self.handle_file(fullpath)
+        #: :class:`list` -- List of dict of the results found in the report.
+        self.vulns = []
+        #: :class:`dict` -- Dict of the metadata found in the report.
+        self.metadata = {}
 
     @classmethod
     def handle_file(cls, fullpath):
         """Process the report file(s) in order to create an i/o stream.
 
-        :param str pathname: Path to the report file.
+        :param str fullpath: Full path to the report file.
 
         :raises: :class:`NotImplementedError` because this is an abstract
             method.
 
         """
         raise NotImplementedError(
-            "A parser MUST define the `handle_file` method.")
+            '`handle_file` function MUST be define for each parser.')
 
     @classmethod
     def is_mine(cls, *args, **kwargs):
         """Check if the parser supports the tool.
 
-        :param list args: Arguments that will be pass to the parser.
-        :param dict kwargs: Arguments that will be pass to the parser.
+        :param list \*args: Arguments that will be passed to the parser.
+        :param dict \*\*kwargs: Arguments that will be passed to the parser.
 
         :raises: :class:`NotImplementedError` because this is an abstract
             method.
 
         """
-        raise NotImplementedError("A parser MUST define the `is_mine` method.")
+        raise NotImplementedError(
+            '`is_mine` function MUST be define for each parser.')
 
     @classmethod
     def check_version(cls, metadata, key='version'):
-        """Checks the version in the metadata against the supported one.
+        """Checks the version in the metadata against the supported one(s).
 
         :param dict metadata: The metadata in which to find the version.
         :param str key: The :attr:`metadata` key containing the version value.
@@ -80,21 +93,27 @@ class AbstractParser(object):
 
         """
         raise NotImplementedError(
-            "A parser MUST define the `parse_metadata` method.")
+            '`parse_metadata` function MUST be define for each parser.')
 
     def parse_report(self):
-        """Parse the discoveries of a report.
+        """Parse the results of a report file(s).
 
         :raises: :class:`NotImplementedError` because this is an abstract
             method.
 
         """
         raise NotImplementedError(
-            "A parser MUST define the `parse_report` method.")
+            '`parse_report` function MUST be define for each parser.')
 
 
 class XMLParser(AbstractParser):
-    """Specialized parser for XML formatted report."""
+
+    """Specialized parser for XML formatted report file(s).
+
+    Define the special :func:`handle_file` function in order to process XML
+    report file(s).
+
+    """
 
     #: str -- XMLParser only supports XML files.
     __format__ = 'xml'
@@ -102,7 +121,7 @@ class XMLParser(AbstractParser):
     def __init__(self, fullpath):
         """Initialize XMLParser.
 
-        :param str pathname: path to the report file.
+        :param str fullpath: full path to the report file.
 
         """
         AbstractParser.__init__(self, fullpath)
@@ -111,9 +130,12 @@ class XMLParser(AbstractParser):
     def handle_file(cls, fullpath):
         """Specialized file handler for XML files.
 
-        :param str pathname: path to the report file.
+        :param str fullpath: path to the report file.
         :raises ValueError: if the report file has not the right extension.
         :raises LxmlError: if Lxml cannot parse the XML file.
+
+        :return: handle on the root node element from the XML file.
+        :rtype: :class:`lxml.etree._Element`
 
         """
         if not fullpath.endswith(cls.__format__):
@@ -123,10 +145,11 @@ class XMLParser(AbstractParser):
 
 
 class FileParser(AbstractParser):
-    """Specialized parser for general report."""
 
-    #: str -- A file can have any extension.
-    __format__ = ''
+    """Specialized parser for generic report file(s).
+
+    Define the special :func:`handle_file` function in order to process generic
+    report file(s).
 
     """
 
@@ -142,11 +165,14 @@ class FileParser(AbstractParser):
     def handle_file(cls, fullpath):
         """Specialized file handler for generic file(s).
 
-        :param str pathname: path to the report file.
+        :param str fullpath: full path to the report file.
         :raises OSError: if an error occurs when opening/reading the report
             file.
         :raises IOError: if an error occurs when opening/reading the report
             file.
+
+        :return: string containing all the data from the file.
+        :rtype: :class:`str`
 
         """
         data = ''
@@ -156,15 +182,22 @@ class FileParser(AbstractParser):
 
 
 class LineParser(AbstractParser):
-    """Specialized parser for general report read line by line."""
+    """Specialized parser for generic report file(s).
 
-    #: str -- A file can have any extension.
-    __format__ = ''
+    Define the special :func:`handle_file` function in order to process generic
+    report file(s).
+
+    .. note::
+
+        Contrary to :class:`FileParser`, this class reads the file line by line
+        and generates a list, instead of a string.
+
+    """
 
     def __init__(self, fullpath):
-        """Initialized LineParser.
+        """Initialize LineParser.
 
-        :param str pathname: path to the report file.
+        :param str fullpath: full path to the report file.
 
         """
         AbstractParser.__init__(self, fullpath)
@@ -173,11 +206,17 @@ class LineParser(AbstractParser):
     def handle_file(cls, fullpath, skip_empty=True):
         """Specialized file handler for general files read line by line.
 
-        :param str pathname: path to the report file.
+        :param str fullpath: path to the report file.
+        :param bool skip_empty: skip the empty lines that can occur in the
+            file if `True`, otherwise keep them.
         :raises OSError: if an error occurs when opening/reading the report
             file.
         :raises IOError: if an error occurs when opening/reading the report
             file.
+
+        :return: list of strings containing all the data from the file, line
+            by line.
+        :rtype: :class:`str`
 
         """
         with open(fullpath, 'r') as f:
