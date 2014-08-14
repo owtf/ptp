715071aa0d6baffab19ee5f8c34a6d8341034737
Tao Sauvage <sauvage.tao@gmail.com>
2014-08-14 03:37:23 +0200

    [exceptions] Update docstrings.

--- libptp/exceptions.py
+++ libptp/exceptions.py
@@ -8,20 +8,20 @@
 
 
 class PTPError(Exception):
-    """General PTP error."""
+    """General :mod:`ptp` error."""
     pass
 
 
 class ReportNotFoundError(PTPError):
-    """PTP error raised when the report file(s) was not found."""
+    """:mod:`ptp` error raised when the report file(s) was not found."""
     pass
 
 
 class NotSupportedToolError(PTPError):
-    """PTP error raised when the tool is not supported"""
+    """:mod:`ptp` error raised when the tool is not supported"""
     pass
 
 
 class NotSupportedVersionError(PTPError):
-    """PTP error raised when the version of the tool is not supported"""
+    """:mod:`ptp` error raised when the version of the tool is not supported"""
     pass
