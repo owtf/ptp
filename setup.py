7850121de98a538ea3c9612497882da68c88150d
Tao Sauvage <sauvage.tao@gmail.com>
2014-08-16 02:21:43 +0200

    [doc] Increase minor version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PTP",
-    version="0.1.0",
+    version="0.1.1",
     description="PTP ranks the discoveries listed in security tool reports.",
     author="Tao Sauvage",
     author_email="sauvage.tao@gmail.com",
