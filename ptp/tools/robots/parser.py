22cb32bac66c89b3c1816c0fe38719424c274dab
Tao Sauvage <sauvage.tao@gmail.com>
2015-08-14 23:58:46 +0200

    squash with previous robots

--- ptp/tools/robots/parser.py
+++ ptp/tools/robots/parser.py
@@ -46,8 +46,6 @@ class RobotsParser(LineParser):
             stream = cls.handle_file(pathname, filename, first=first)
         except (OSError, IOError, ValueError):
             return False
-        print("@@@ DEBUG")
-        print(stream)
         if stream and stream[0].startswith(('User-agent:', 'Disallow:', 'Allow:')):
             return True
         return False
