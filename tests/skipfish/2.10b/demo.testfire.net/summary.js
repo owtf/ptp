fd7ec66845290ef91e07fc8dad5cb6ebcbd41fad
Tao Sauvage <sauvage.tao@gmail.com>
2016-04-18 21:37:40 +0200

    [unittest] Remove old homebrew unittests.

--- tests/skipfish/2.10b/demo.testfire.net/summary.js
+++
@@ -1,4 +0,0 @@
-var sf_version = '2.10b';
-var scan_date  = 'Mon May 19 20:32:47 2014';
-var scan_seed  = '0x4491d352';
-var scan_ms    = 2494666;
--- tests/skipfish/2.10b/local.xss/summary.js
+++
@@ -1,4 +0,0 @@
-var sf_version = '2.10b';
-var scan_date  = 'Tue May 27 11:31:14 2014';
-var scan_seed  = '0x5b37a9a3';
-var scan_ms    = 44836;
