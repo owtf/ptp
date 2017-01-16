35330c47b58f8b320c2fcdd3f1fd7f48b9275fb8
Tao Sauvage <sauvage.tao@gmail.com>
2017-01-17 00:02:20 +0900

    [pip] Update setup.py.

--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ from setuptools import setup, find_packages
 setup(
     name="PTP",
     version="0.4.0",
-    description="PTP ranks the discoveries listed in security tool reports.",
+    description="PTP parses and ranks the discoveries listed in security tool reports.",
     author="Tao Sauvage",
     author_email="sauvage.tao@gmail.com",
     license="BSD",
     keywords="PTP Security Automated Ranking",
-    url="http://owtf.github.io/ptp",
+    url="https://owtf.github.io/ptp",
     packages=find_packages(exclude=['docs']),
     use_2to3=True,
-    install_requires=["lxml", "js2py"],
-)
+    install_requires=["lxml", "js2py"],)
